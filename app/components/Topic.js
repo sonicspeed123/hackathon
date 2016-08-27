fac2850ab22e75c28f9bbc2c120ef7501423a582
packercj <packerc@gmail.com>
2016-08-27 11:08:46 -0600

    adding topic component

---
+++ app/components/Topic.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import $ from 'jquery';
+
+
+class Topic extends React.Component {
+  render() {
+      return (
+        <div>
+         <p>this is working</p>
+         <a href="../components/item">Item</a>
+        </div>
+      )
+    }
+  }
+
+  export default Topic;
--- app/containers/Topic.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import $ from 'jquery';
-import 
-
-class
