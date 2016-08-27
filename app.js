837a09e3d78b101300f73800266e6e7e875aa0d7
sonicspeed123 <joshuasahagun1996@yahoo.com>
2016-08-27 09:42:00 -0600

    rename to hackathon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect( 'mongodb://localhost/react-starter' );
+mongoose.connect( 'mongodb://localhost/hackathon' );
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
